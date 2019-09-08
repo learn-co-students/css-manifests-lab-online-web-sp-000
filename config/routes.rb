d60da3d1624b8dba0730cdfaade305362f733268
Barbara Johnson <johnson.barbara@gmail.com>
2019-09-07 23:49:08 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/admin', to: 'home#admin'
-  get '/index', to: 'home#index'
 end
